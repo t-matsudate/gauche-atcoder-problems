b61f92c5a44753b77c99150557b29e2f8ced6a79
松舘 剛志 <41981585+t-matsudate@users.noreply.github.com>
2024-05-27 05:18:08 +0000

    Fixed: `integer->digit` は2桁以上の数値を扱えない(そう言われればそれはそう)。

--- abc347a.scm
+++ abc347a.scm
@@ -10,10 +10,6 @@
             (cons (quotient item k) (map-by-divisible k (cdr numbers)))
             (map-by-divisible k (cdr numbers))))))
 
-; NOTE:  いくつかのケースで実行時エラーが発生する。 5/27 時点で原因は不明。
-;        ここで、提出時の実行時間と使用メモリ量それぞれの平均は以下の通り。
-;        実行時間: 11ms
-;        使用メモリ量: 12MB
 (define (abc347a)
   (let ((n+k (map string->number
                   (string-split (read-line (current-input-port))
@@ -21,4 +17,4 @@
         (numbers (map string->number
                       (string-split (read-line (current-input-port))
                                     " "))))
-    (print (list->string (intersperse #\space (map integer->digit (map-by-divisible (list-ref n+k 1) numbers)))))))
+    (apply print (intersperse #\space (map-by-divisible (list-ref n+k 1) numbers)))))
