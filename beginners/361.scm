eea12874ab2ea6499f2f6642e7700d070ef8a9a6
松舘 剛志 <41981585+t-matsudate@users.noreply.github.com>
2024-07-10 04:48:28 +0000

    Added: ABC361B

--- beginners/361.scm
+++ beginners/361.scm
@@ -1,5 +1,6 @@
 (define-module beginners.361
-  (export abc361a))
+  (export abc361a
+          abc361b))
 (select-module beginners.361)
 
 (define (insert-number numbers index number)
@@ -19,3 +20,29 @@
                         (insert-number numbers
                                        (list-ref preconditions 1)
                                        (list-ref preconditions 2))))))
+
+(define (intersected-cubic? c1 c2)
+  (define (intersects? p1 p2 p3 p4)
+    (and (> p2 p3)
+         (> p4 p1)))
+  (let* ((c1-start (take c1 3))
+         (c1-end (drop c1 3))
+         (c2-start (take c2 3))
+         (c2-end (drop c2 3)))
+    (and ; (x1, x2, x3, x4)
+         (intersects? (list-ref c1-start 0) (list-ref c1-end 0) (list-ref c2-start 0) (list-ref c2-end 0))
+         ; (y1, y2, y3, y4)
+         (intersects? (list-ref c1-start 1) (list-ref c1-end 1) (list-ref c2-start 1) (list-ref c2-end 1))
+         ; (z1, z2, z3, z4)
+         (intersects? (list-ref c1-start 2) (list-ref c1-end 2) (list-ref c2-start 2) (list-ref c2-end 2)))))
+
+(define (abc361b)
+  (let* ((c1 (map string->number
+                  (string-split (read-line (current-input-port))
+                                " ")))
+         (c2 (map string->number
+                  (string-split (read-line (current-input-port))
+                                " "))))
+    (if (intersected-cubic? c1 c2)
+        (print "Yes")
+        (print "No"))))
