1c1797a516cd6bb37fcf09bbe67dcbb485792ecb
松舘 剛志 <41981585+t-matsudate@users.noreply.github.com>
2024-07-10 05:36:14 +0000

    Added: ABC357B

--- beginners/357.scm
+++ beginners/357.scm
@@ -1,5 +1,6 @@
 (define-module beginners.357
-  (export abc357))
+  (export abc357a
+          abc357b))
 (select-module beginners.357)
 
 (define (count-sanitized remained aliens)
@@ -12,7 +13,7 @@
               (go (+ ret 1) (- remained alien) (cdr aliens))))))
   (go 0 remained aliens))
 
-(define (abc357)
+(define (abc357a)
   (let ((preconditions (map string->number
                             (string-split (read-line (current-input-port))
                                           " ")))
@@ -20,3 +21,14 @@
                    (string-split (read-line (current-input-port))
                                  " "))))
     (print (count-sanitized (list-ref preconditions 1) line))))
+
+(define (adjusted-case line)
+  (let ((uppers (count char-upper-case? (string->list line)))
+        (lowers (count char-lower-case? (string->list line))))
+    (if (> uppers lowers)
+        (string-map char-upcase line)
+        (string-map char-downcase line))))
+
+(define (abc357b)
+  (let ((line (read-line (current-input-port))))
+    (print (adjusted-case line))))
--- main.scm
+++ main.scm
@@ -1,2 +1,2 @@
 (add-load-path ".")
-(use beginners.361)
+(use beginners.357)
