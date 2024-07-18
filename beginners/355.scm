fd88ddf336f5e01084c3f4f154f70974e452d55e
松舘 剛志 <41981585+t-matsudate@users.noreply.github.com>
2024-07-18 04:18:04 +0000

    Added: ABC355B

--- beginners/355.scm
+++ beginners/355.scm
@@ -1,5 +1,6 @@
 (define-module beginners.355
-  (export abc355))
+  (export abc355a
+          abc355b))
 (select-module beginners.355)
 
 (define candidates '(1 2 3))
@@ -11,9 +12,30 @@
         -1
         (car remained))))
 
-(define (abc355)
+(define (abc355a)
   (let ((line (map string->number
                    (string-split (read-line (current-input-port))
                                  " "))))
     (print (find-criminal (list-ref line 0)
                           (list-ref line 1)))))
+
+(define (consecutive? numbers sub)
+  (cond ((or (< (length sub) 2) (< (length numbers) 2)) #f)
+        ((> (list-ref sub 0) (list-ref numbers 0)) (consecutive? (cdr numbers) sub))
+        ((< (list-ref sub 0) (list-ref numbers 0)) (consecutive? numbers (cdr sub)))
+        (else (or (= (list-ref sub 1) (list-ref numbers 1))
+                  (consecutive? (cdr numbers) (cdr sub))))))
+
+(define (abc355b)
+  (let* ((preconditions (map string->number
+                             (string-split (read-line (current-input-port))
+                                           " ")))
+         (sub (map string->number
+                   (string-split (read-line (current-input-port))
+                                 " ")))
+         (numbers (map string->number
+                       (string-split (read-line (current-input-port))
+                                     " "))))
+    (if (consecutive? (sort (append numbers sub) <) (sort sub <))
+        (print "Yes")
+        (print "No"))))
