5f930696ee1d572834229686f56685832b14a1bc
松舘 剛志 <41981585+t-matsudate@users.noreply.github.com>
2024-07-27 11:49:55 +0000

    Added: ABC362B

--- beginners/362.scm
+++ beginners/362.scm
@@ -1,5 +1,6 @@
 (define-module beginners.362
-  (export abc362a))
+  (export abc362a
+          abc362b))
 (select-module beginners.362)
 
 (define (minimum-except-one rgb except)
@@ -13,3 +14,29 @@
                                  " ")))
          (except (read-line (current-input-port))))
     (print (minimum-except-one rgb except))))
+
+(define (compute-length p1 p2)
+  (+ (square (- (list-ref p2 0) (list-ref p1 0)))
+     (square (- (list-ref p2 1) (list-ref p1 1)))))
+
+(define (right-angle? p1 p2 p3)
+  (let ((a (compute-length p1 p2))
+        (b (compute-length p2 p3))
+        (c (compute-length p3 p1)))
+    (or (= a (+ b c))
+        (= b (+ a c))
+        (= c (+ a b)))))
+
+(define (abc362b)
+  (let* ((p1 (map string->number
+                  (string-split (read-line (current-input-port))
+                                " ")))
+         (p2 (map string->number
+                  (string-split (read-line (current-input-port))
+                                " ")))
+         (p3 (map string->number
+                  (string-split (read-line (current-input-port))
+                                " "))))
+    (if (right-angle? p1 p2 p3)
+        (print "Yes")
+        (print "No"))))
