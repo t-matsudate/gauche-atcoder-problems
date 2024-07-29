85699d17a6b413705270f25821b1079d9ca0cdca
松舘 剛志 <41981585+t-matsudate@users.noreply.github.com>
2024-07-29 03:39:42 +0000

    Added: ABC363B

--- beginners/363.scm
+++ beginners/363.scm
@@ -1,5 +1,6 @@
 (define-module beginners.363
-  (export abc363a))
+  (export abc363a
+          abc363b))
 (select-module beginners.363)
 
 (define (next-caret rate)
@@ -11,3 +12,42 @@
 (define (abc363a)
   (let ((rate (string->number (read-line (current-input-port)))))
     (print (next-caret rate))))
+
+(define (take-while pred sequence)
+  (if (null? sequence)
+      '()
+      (let ((item (car sequence)))
+        (if (pred item)
+            (cons item (take-while pred (cdr sequence)))
+            '()))))
+
+(define (drop-while pred sequence)
+  (if (null? sequence)
+      '()
+      (let ((item (car sequence)))
+        (if (pred item)
+            (drop-while pred (cdr sequence))
+            sequence))))
+
+(define (exceeds-after len count dolls)
+  (define (go ret len count dolls)
+    (if (or (null? dolls) (<= count 0))
+        ret
+        (go (max ret (- len (car dolls)))
+            len
+            (- count 1)
+            (cdr dolls))))
+  (let ((exceeded (take-while (lambda (doll) (>= doll len)) dolls))
+        (remained (drop-while (lambda (doll) (>= doll len)) dolls)))
+    (go 0 len (- count (length exceeded)) remained)))
+
+(define (abc363b)
+  (let* ((preconditions (map string->number
+                             (string-split (read-line (current-input-port))
+                                           " ")))
+         (dolls (map string->number
+                     (string-split (read-line (current-input-port))
+                                   " "))))
+    (print (exceeds-after (list-ref preconditions 1)
+                          (list-ref preconditions 2)
+                          (sort dolls >)))))
