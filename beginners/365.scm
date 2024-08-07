704e5c55b43168794e88c95358ae514f0c7363be
松舘 剛志 <41981585+t-matsudate@users.noreply.github.com>
2024-08-07 01:16:18 +0000

    Added: ABC365B

--- beginners/365.scm
+++ beginners/365.scm
@@ -1,5 +1,6 @@
 (define-module beginners.365
-  (export abc365a))
+  (export abc365a
+          abc365b))
 (select-module beginners.365)
 
 (define (days-of-year year)
@@ -11,3 +12,21 @@
 (define (abc365a)
   (let ((year (string->number (read-line (current-input-port)))))
     (print (days-of-year year))))
+
+(define (index-of-second-higher numbers)
+  (define sorted (sort numbers >))
+  (define second-higher (list-ref sorted 1))
+  (define (go numbers)
+    (if (null? numbers)
+        0
+        (if (= second-higher (car numbers))
+            1
+            (+ 1 (go (cdr numbers))))))
+  (go numbers))
+
+(define (abc365b)
+  (let* ((n (string->number (read-line (current-input-port))))
+         (numbers (map string->number
+                       (string-split (read-line (current-input-port))
+                                     " "))))
+    (print (index-of-second-higher numbers))))
